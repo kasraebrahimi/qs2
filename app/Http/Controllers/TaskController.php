4c893105e915e95728121fad41973c50cb75e2c7
kasraebrahimi <ebrahimi.kasra1989@gmail.com>
2019-05-23 12:07:51 +0430

    view tasks.index added and routed

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
-    //
+    public function __construct()
+    {
+      $this->middleware('auth');
+    }
+
+    public function index(Request $request)
+    {
+      return view('tasks.index');
+    }
 }
